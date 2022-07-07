910fc3941072e7080a199aba2b9a67b222904657
Yoel Larrosa <Yoel.larrosa@hotmail.com>
2022-07-07 16:51:37 -0300

    Casi finalizado
    Muchos retoques y links funcionando

--- script.js
+++ script.js
@@ -23,19 +23,3 @@ function responsiveMenu(){
         x.className = "";
     }
 }
-
-window.onscroll = function(){
-    efectoHabilidades();
-};
-
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-
-    if(distancia_skills>= 200){
-        document.getElementById("html").classList.add("barra-progreso1");
-        document.getElementById("js").classList.add("barra-progreso2");
-        document.getElementById("bd").classList.add("barra-progreso3");
-        document.getElementById("ps").classList.add("barra-progreso4");
-    }
-};
